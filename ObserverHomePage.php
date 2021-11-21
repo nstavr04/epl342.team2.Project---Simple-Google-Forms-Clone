5ad82b0e66b76110e529e43bfd9aae88088cd9bd
Nikolas Stavrou <nstavr04@ucy.ac.cy>
2021-11-21 20:21:10 +0200

    added the accordions on observer

---
+++ ObserverHomePage.php
@@ -0,0 +1,273 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Font Awesome -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+	<!-- MDB -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.0/mdb.min.css" rel="stylesheet"/>
+
+    <title>Observer Home Page</title>
+  </head>
+
+  <body>
+
+        <!-- Titles -->
+        <div class="text-center page-header">
+                <br>
+                <h2>Welcome to Observer Home Page</h2>
+                <hr>
+        </div>
+
+        <div class="text-center page-header w-50 " style="margin-left: 25.5%;">
+            <h3>Pick one of the options below</h3>
+            <hr>
+            <br>
+        </div>
+
+        <!-- Accordion with options and forms in it -->
+    
+        <div class="accordion p-3" id="accordion">
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                <button
+                    class="accordion-button"
+                    type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                >
+                    Add Company and Company Manager
+                </button>
+                </h2>
+                <div
+                id="collapseOne"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingOne"
+                data-mdb-parent="#accordionExample"
+                >
+                <div class="accordion-body">
+                    <strong>This is the first item's accordion body.</strong> It is hidden by default,
+                    until the collapse plugin adds the appropriate classes that we use to style each
+                    element. These classes control the overall appearance, as well as the showing and
+                    hiding via CSS transitions. You can modify any of this with custom CSS or
+                    overriding our default variables. It's also worth noting that just about any HTML
+                    can go within the <strong>.accordion-body</strong>, though the transition does
+                    limit overflow.
+                </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#collapseTwo"
+                    aria-expanded="false"
+                    aria-controls="collapseTwo"
+                >
+                    View details of a Company
+                </button>
+                </h2>
+                <div
+                id="collapseTwo"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingTwo"
+                data-mdb-parent="#accordionExample"
+                >
+                <div class="accordion-body">
+                    <strong>This is the second item's accordion body.</strong> It is hidden by
+                    default, until the collapse plugin adds the appropriate classes that we use to
+                    style each element. These classes control the overall appearance, as well as the
+                    showing and hiding via CSS transitions. You can modify any of this with custom CSS
+                    or overriding our default variables. It's also worth noting that just about any
+                    HTML can go within the <strong>.accordion-body</strong>, though the transition
+                    does limit overflow.
+                </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#collapseThree"
+                    aria-expanded="false"
+                    aria-controls="collapseThree"
+                >
+                    Modify details of a Company
+                </button>
+                </h2>
+                <div
+                id="collapseThree"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingThree"
+                data-mdb-parent="#accordionExample"
+                >
+                <div class="accordion-body">
+                    <strong>This is the third item's accordion body.</strong> It is hidden by default,
+                    until the collapse plugin adds the appropriate classes that we use to style each
+                    element. These classes control the overall appearance, as well as the showing and
+                    hiding via CSS transitions. You can modify any of this with custom CSS or
+                    overriding our default variables. It's also worth noting that just about any HTML
+                    can go within the <strong>.accordion-body</strong>, though the transition does
+                    limit overflow.
+                </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingFour">
+                <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#collapseFour"
+                    aria-expanded="false"
+                    aria-controls="collapseFour"
+                >
+                    View details of a Company Manager or User
+                </button>
+                </h2>
+                <div
+                id="collapseFour"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingFour"
+                data-mdb-parent="#accordionExample"
+                >
+                <div class="accordion-body">
+                    <strong>This is the third item's accordion body.</strong> It is hidden by default,
+                    until the collapse plugin adds the appropriate classes that we use to style each
+                    element. These classes control the overall appearance, as well as the showing and
+                    hiding via CSS transitions. You can modify any of this with custom CSS or
+                    overriding our default variables. It's also worth noting that just about any HTML
+                    can go within the <strong>.accordion-body</strong>, though the transition does
+                    limit overflow.
+                </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingFive">
+                <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#collapseFive"
+                    aria-expanded="false"
+                    aria-controls="collapseFive"
+                >
+                    Modify details of a Company Manager
+                </button>
+                </h2>
+                <div
+                id="collapseFive"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingFive"
+                data-mdb-parent="#accordionExample"
+                >
+                <div class="accordion-body">
+                    <strong>This is the third item's accordion body.</strong> It is hidden by default,
+                    until the collapse plugin adds the appropriate classes that we use to style each
+                    element. These classes control the overall appearance, as well as the showing and
+                    hiding via CSS transitions. You can modify any of this with custom CSS or
+                    overriding our default variables. It's also worth noting that just about any HTML
+                    can go within the <strong>.accordion-body</strong>, though the transition does
+                    limit overflow.
+                </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSix">
+                <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#collapseSix"
+                    aria-expanded="false"
+                    aria-controls="collapseSix"
+                >
+                    View details of a certain question
+                </button>
+                </h2>
+                <div
+                id="collapseSix"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingSix"
+                data-mdb-parent="#accordionExample"
+                >
+                <div class="accordion-body">
+                    <strong>This is the third item's accordion body.</strong> It is hidden by default,
+                    until the collapse plugin adds the appropriate classes that we use to style each
+                    element. These classes control the overall appearance, as well as the showing and
+                    hiding via CSS transitions. You can modify any of this with custom CSS or
+                    overriding our default variables. It's also worth noting that just about any HTML
+                    can go within the <strong>.accordion-body</strong>, though the transition does
+                    limit overflow.
+                </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSeven">
+                <button
+                    class="accordion-button collapsed"
+                    type="button"
+                    data-mdb-toggle="collapse"
+                    data-mdb-target="#collapseSeven"
+                    aria-expanded="false"
+                    aria-controls="collapseSeven"
+                >
+                View details of a certain questionnaire
+                </button>
+                </h2>
+                <div
+                id="collapseSeven"
+                class="accordion-collapse collapse"
+                aria-labelledby="headingSeven"
+                data-mdb-parent="#accordionExample"
+                >
+                <div class="accordion-body">
+                    <strong>This is the third item's accordion body.</strong> It is hidden by default,
+                    until the collapse plugin adds the appropriate classes that we use to style each
+                    element. These classes control the overall appearance, as well as the showing and
+                    hiding via CSS transitions. You can modify any of this with custom CSS or
+                    overriding our default variables. It's also worth noting that just about any HTML
+                    can go within the <strong>.accordion-body</strong>, though the transition does
+                    limit overflow.
+                </div>
+                </div>
+            </div>
+        </div>
+
+        <br>
+
+        <!-- Last Button to view the company manager and simple user options -->
+        <div class="p-3">
+            <button type="submit" class="btn btn-primary btn-block">View Company Manager and Simple User Options</button>
+            <br>
+            <div class="w-50" style="margin-left: 25.5%;">
+            <button type="submit" name="disconnect" class="btn btn-primary btn-block">Disconnect</button>
+            </div>
+        </div>
+
+    <?php
+		if(isset($_POST['disconnect'])) { 
+			echo "Clossing session and redirecting to start page"; 
+			session_unset();
+			session_destroy();
+			die('<meta http-equiv="refresh" content="2; url=index.php" />');
+		} 
+	?> 
+
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.0/mdb.min.js"></script>
+    </body>
+</html>
